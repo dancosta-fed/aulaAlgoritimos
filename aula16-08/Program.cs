ad6df450f489cd65f7e06a26ce16909c3c80b363
Daniel Costa <daniel@saudeagora.com.vc>
2023-08-16 21:43:42 -0300

    first commit

---
+++ aula16-08/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int num = 2;
+
+        Console.WriteLine($"Hello, World! {num}");
+        Console.WriteLine("Hello World!");
+
+        Console.ReadLine();
+    }
+} 
